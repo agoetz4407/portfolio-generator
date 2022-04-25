3f9ee5f37eaff22957f26b13858a09ba1d5b7698
Andrew Goetz <agoetz4407@gmail.com>
2022-04-24 19:55:51 -0500

    using promises, modules

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
 const generatePage = require('./src/page-template.js');
+const {writeFile, copyFile} = require('./utils/generate-site.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -130,60 +130,11 @@ Add A New Project
 
 promptUser()
     .then(promptProject)
-    .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile('index.html', pageHTML, err => {
-        if (err) throw new Error(err);
-
-        console.log('Portfolio complete! Check index.html for the output.')
-        })
+    .then(portfolioData => generatePage(portfolioData))
+    .then(pageHTML => writeFile(pageHTML))
+    .then(writeFileResponse => {
+        console.log(writeFileResponse)
+        return copyFile()
     })
-
-// const mockData = {
-//     name: 'Lernantino',
-//     github: 'lernantino',
-//     confirmAbout: true,
-//     about:
-//       'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
-//     projects: [
-//       {
-//         name: 'Run Buddy',
-//         description:
-//           'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-//         tools: ['HTML', 'CSS'],
-//         link: 'https://github.com/lernantino/run-buddy',
-//         feature: true,
-//         confirmAddProject: true
-//       },
-//       {
-//         name: 'Taskinator',
-//         description:
-//           'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-//         tools: ['JavaScript', 'HTML', 'CSS'],
-//         link: 'https://github.com/lernantino/taskinator',
-//         feature: true,
-//         confirmAddProject: true
-//       },
-//       {
-//         name: 'Taskmaster Pro',
-//         description:
-//           'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-//         tools: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
-//         link: 'https://github.com/lernantino/taskmaster-pro',
-//         feature: false,
-//         confirmAddProject: true
-//       },
-//       {
-//         name: 'Robot Gladiators',
-//         description:
-//           'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
-//         tools: ['JavaScript'],
-//         link: 'https://github.com/lernantino/robot-gladiators',
-//         feature: false,
-//         confirmAddProject: false
-//       }
-//     ]
-//   };
-
-// const pageHTML = generatePage(mockData)
+    .then(copyFileResponse => console.log(copyFileResponse))
+    .catch(err => console.log(err))
